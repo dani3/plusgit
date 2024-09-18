2053c1253b1072c05e4628470b8551084a2d998f
Daniel Mancebo <daniel.m.aldea@hotmail.com>
2024-09-18 12:17:28 +0200

    Initialized basic CLI

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use anyhow::Error;
+use clap::{Arg, Command};
+
+mod init;
+
+fn cli() -> Command {
+    Command::new("git")
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .subcommand(
+            Command::new("init")
+                .about("Create an empty Git repository")
+                .arg(Arg::new("path").default_value(".")),
+        )
+}
+
+fn main() -> Result<(), Error> {
+    let matches = cli().get_matches();
+
+    match matches.subcommand() {
+        Some(("init", sub_matches)) => {
+            init::run(sub_matches.get_one::<String>("path").unwrap());
+        }
+        _ => unreachable!(),
+    }
+
+    Ok(())
+}
